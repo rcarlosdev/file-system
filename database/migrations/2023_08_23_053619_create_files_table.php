1d1afe6002317fb7999194a082a80eed4a174ad1
Carlos Restrepo <carl.rest19@gmail.com>
2023-08-23 01:15:46 -0500

    migraciones

--- database/migrations/2023_08_23_053619_create_files_table.php
+++ database/migrations/2023_08_23_053619_create_files_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,7 +14,16 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 1024);
+            $table->string('path', 1024)->nullable();
+            $table->nestedSet();
+            $table->boolean('is_folder');
+            $table->string('mime')->nullable();
+            $table->integer('size')->nullable();
             $table->timestamps();
+            $table->foreignIdFor(User::class, 'created_by');
+            $table->foreignIdFor(User::class, 'updated_by');
+            $table->softDeletes();
         });
     }
 
